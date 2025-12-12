15e45bf74123bd7142b80c6869afb01484f7cfc2
Vijju-1704 <vijayrakeshanugula@gmail.com>
2025-12-12 10:09:20 +0530

    new

--- Repositories/IUnitOfWork.cs
+++ Repositories/IUnitOfWork.cs
@@ -3,10 +3,7 @@
     public interface IUnitOfWork : IDisposable
     {
         IUserRepository Users { get; }
-        // Add other repositories here as your application grows
-        // IProductRepository Products { get; }
-        // IOrderRepository Orders { get; }
-
+        
         Task<int> SaveChangesAsync();
         Task<bool> SaveAsync();
     }
